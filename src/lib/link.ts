7a773aa6653181f50a66ad036008968eb055951a
Alena Batitskaia <batickaya.a@gmail.com>
2026-03-31 14:47:36 +0100

    chore: bump version to 2.0.23 and update UTM parameter from "purple" to "banner"

--- src/lib/link.ts
+++ src/lib/link.ts
@@ -3,7 +3,7 @@ import { getDate } from "./date";
 function setUTM(link: string, source: string, campaign: string | null) {
   const url = new URL(link);
 
-  url.searchParams.set("utm_medium", "purple");
+  url.searchParams.set("utm_medium", "banner");
   url.searchParams.set("utm_source", source);
   url.searchParams.set(
     "utm_campaign",
